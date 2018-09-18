f2bf3ef4a69dc4420b65dd7c922ff2fcf3874924
Samuel Useche <samueluseche87@gmail.com>
2018-09-18 16:23:35 -0500

    fix error in migration

--- database/migrations/2018_09_17_000001_create_config_stripe_table.php
+++ database/migrations/2018_09_17_000001_create_config_stripe_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateStripeConfig extends Migration {
+class CreateConfigStripeTable extends Migration {
 
 	/**
 	 * Run the migrations.
